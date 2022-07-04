63105ea22f5edcf230979a798b4de80ce63bca71
Y.Hun <huyeon123@naver.com>
2022-07-04 10:15:58 +0900

    Add Spring Security

---
+++ server/src/main/java/com/huyeon/apiserver/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.huyeon.apiserver.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+}
