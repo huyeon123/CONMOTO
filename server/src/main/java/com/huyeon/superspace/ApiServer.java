b918335b486e53e529a7e0d37db182d6f2f06d85
Y.Hun <ploki753@gmail.com>
2022-11-02 16:46:22 +0900

    feat: Add Spring Cloud Config Dependencies & Adjust YML

--- server/src/main/java/com/huyeon/superspace/ApiServer.java
+++ server/src/main/java/com/huyeon/superspace/ApiServer.java
@@ -1,12 +1,10 @@
 package com.huyeon.superspace;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@EnableEncryptableProperties
 @SpringBootApplication
 public class ApiServer {
     public static void main(String[] args) {
