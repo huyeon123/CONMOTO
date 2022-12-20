a1ce3f8cbf1683024593830c741cb340b1a68e60
Y.Hun <ploki753@gmail.com>
2022-12-20 16:52:25 +0900

    feat: Add refreshAccessToken AOP

---
+++ front-server/src/main/java/com/huyeon/frontend/aop/refreshAccessTokenAop.java
@@ -0,0 +1,74 @@
+package com.huyeon.frontend.aop;
+
+import com.huyeon.frontend.jwt.TokenExtractor;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class refreshAccessTokenAop {
+    private static final String AUTH_SERVER_ADDR = "http://localhost:8100/auth";
+    public static final String REFRESH_KEY = "Super-Space-Refresh";
+
+    private final TokenExtractor tokenExtractor;
+
+    @Pointcut("within(com.huyeon.frontend.controller..*) && !@annotation(PurePage)")
+    public void needSideBarAndHeader() {
+
+    }
+
+    @Around("needSideBarAndHeader()")
+    private Object refreshAccessToken(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        String refreshToken = (String) args[0];
+        args[1] = generateNewAccessToken(refreshToken);
+
+        if (args[1] == null) {
+            return "redirect:/";
+        }
+
+        return joinPoint.proceed(args);
+    }
+
+    private String generateNewAccessToken(String refreshToken) {
+        if (refreshToken != null) {
+            String resolveRefreshToken = resolveRefreshToken(refreshToken);
+            if (isNotValidToken(resolveRefreshToken)) return null;
+        }
+        RestTemplate restTemplate = new RestTemplate();
+
+        return restTemplate.exchange(
+                AUTH_SERVER_ADDR + "/refresh",
+                HttpMethod.GET,
+                getRequestBody(refreshToken),
+                String.class
+        ).getBody();
+    }
+
+    private String resolveRefreshToken(String refreshToken) {
+        int delimiterIndex = refreshToken.indexOf("-");
+        return refreshToken.substring(delimiterIndex + 1);
+    }
+
+    private boolean isNotValidToken(String token) {
+        return token == null || !tokenExtractor.validateToken(token);
+    }
+
+    private HttpEntity<?> getRequestBody(String refreshToken) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.set(REFRESH_KEY, refreshToken);
+
+        return new HttpEntity<>(refreshToken, httpHeaders);
+    }
+}
---
+++ front-server/src/main/java/com/huyeon/frontend/jwt/TokenExtractor.java
@@ -0,0 +1,65 @@
+package com.huyeon.frontend.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Slf4j
+@Component
+public class TokenExtractor implements InitializingBean {
+    private final String secret;
+    private Key key;
+
+    /**
+     * Bean이 생성되고 생성자에서 yaml 파일에 설정해 놓은 값을 주입 받은 후
+     * secret값을 Base64로 Decode해 key변수에 할당 (afterPropertiesSet)
+     */
+    public TokenExtractor(
+            @Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            parseClaims(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+
+    private Jws<Claims> parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+    }
+
+    public String getSubject(String token) {
+        Claims claims = getClaim(token);
+        return claims.getSubject();
+    }
+
+    private Claims getClaim(String token) {
+        return parseClaims(token).getBody();
+    }
+}
