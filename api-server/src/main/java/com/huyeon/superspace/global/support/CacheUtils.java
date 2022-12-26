d703602df3cb3011a2ff7ce8844190a72fd22125
Y.Hun <ploki753@gmail.com>
2022-12-26 16:18:11 +0900

    feat: Add CacheUtils Class with RestTemplate

--- api-server/src/main/java/com/huyeon/superspace/global/support/CacheUtils.java
+++ api-server/src/main/java/com/huyeon/superspace/global/support/CacheUtils.java
@@ -1,2 +1,33 @@
-package com.huyeon.superspace.global.support;public class CacheUtils {
+package com.huyeon.superspace.global.support;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class CacheUtils {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public Map<String, Object> findCache(String key) {
+        HashOperations<String, String, Object> ops = redisTemplate.opsForHash();
+        return ops.entries(key);
+    }
+
+    public Object findCache(String key, String hashKey) {
+        HashOperations<String, String, Object> ops = redisTemplate.opsForHash();
+        return ops.get(key, hashKey);
+    }
+
+    public void saveCache(String key, Map<String, Object> value) {
+        redisTemplate.opsForHash().putAll(key, value);
+    }
+
+    public void setExpire(String key, long timeout, TimeUnit timeUnit) {
+        redisTemplate.expire(key, timeout, timeUnit);
+    }
 }
