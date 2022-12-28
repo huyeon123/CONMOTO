0104dd1f033f44cc9ea998e866f5b87c202ed4ef
Y.Hun <ploki753@gmail.com>
2022-12-28 10:58:19 +0900

    feat: Add CategoryName Validation

--- front-server/src/main/resources/static/js/category/category-create.js
+++ front-server/src/main/resources/static/js/category/category-create.js
@@ -2,6 +2,9 @@ async function createCategory() {
     const url = "/api/category?groupUrl=" + groupUrl;
     const categoryName = $('#target-category-name').val();
     const parentCategoryId = $('#parent-category option:selected').val();
+
+    if (hasInvalidWord(categoryName)) return;
+
     const request = {
         name: categoryName,
         categoryId: parentCategoryId
@@ -13,4 +16,13 @@ async function createCategory() {
             location.href = "/workspace/" + groupUrl;
         }
     }).catch(error => console.error(error));
+}
+
+function hasInvalidWord(categoryName) {
+    const regex = "[\\w\\s!@#$%^&*()-.,;:\'\"\[\]{}]+";
+    if (!regex.test(categoryName)) {
+        alert("해당 이름은 사용할 수 없습니다.");
+        return true;
+    }
+    return false;
 }
