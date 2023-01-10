33f6dfafcf993dfad170c651799512c1da82b8ba
Y.Hun <ploki753@gmail.com>
2023-01-10 19:25:15 +0900

    feat: Add EnableMongoAuditing Annotation

--- api-server/src/main/java/com/huyeon/superspace/ApiServer.java
+++ api-server/src/main/java/com/huyeon/superspace/ApiServer.java
@@ -3,8 +3,10 @@ package com.huyeon.superspace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @EnableJpaAuditing
+@EnableMongoAuditing
 @SpringBootApplication
 public class ApiServer {
     public static void main(String[] args) {
