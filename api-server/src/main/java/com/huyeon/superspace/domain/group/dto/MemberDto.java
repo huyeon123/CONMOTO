a25568ededc440547b189ca49cdf67bbd3e37fa4
Y.Hun <ploki753@gmail.com>
2023-01-16 16:50:37 +0900

    feat: New Group/Member Service with NoSQL
    - MongoRepository를 이용한 Service 코드 작성
    - 기존 Group Service와 Member Service를 한번에 작성

--- api-server/src/main/java/com/huyeon/superspace/domain/group/dto/MemberDto.java
+++ api-server/src/main/java/com/huyeon/superspace/domain/group/dto/MemberDto.java
@@ -1,19 +1,30 @@
 package com.huyeon.superspace.domain.group.dto;
 
-import com.huyeon.superspace.domain.user.entity.User;
+import com.huyeon.superspace.domain.group.document.Member;
 import lombok.*;
 
 @Getter
+@Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberDto {
-    private String name;
-    private String email;
-    private String groupRole;
+    private String id;
 
-    public MemberDto(User user, String groupRole) {
-        name = user.getName();
-        email = user.getEmail();
-        this.groupRole = groupRole;
+    private String email;
+
+    private String nickname;
+
+    private String groupUrl;
+
+    private String role;
+
+    public MemberDto(Member member) {
+        id = member.getGroup().getId();
+        email = member.getUserEmail();
+        nickname = member.getNickname();
+        groupUrl = member.getGroup().getUrl();
+        role = member.getRole();
     }
 }
+
