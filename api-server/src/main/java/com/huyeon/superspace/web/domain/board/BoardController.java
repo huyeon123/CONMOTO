9f76f5d34f37158276e601699b8eee231396df87
Y.Hun <ploki753@gmail.com>
2023-01-16 16:42:47 +0900

    refactor: Apply New Service

--- api-server/src/main/java/com/huyeon/superspace/web/domain/board/BoardController.java
+++ api-server/src/main/java/com/huyeon/superspace/web/domain/board/BoardController.java
@@ -2,12 +2,10 @@ package com.huyeon.superspace.web.domain.board;
 
 import com.huyeon.superspace.domain.board.dto.BoardDto;
 import com.huyeon.superspace.domain.board.dto.CommentDto;
-import com.huyeon.superspace.domain.board.dto.ContentDto;
-import com.huyeon.superspace.domain.board.dto.TagDto;
-import com.huyeon.superspace.domain.board.service.BoardService;
-import com.huyeon.superspace.domain.board.service.CommentService;
-import com.huyeon.superspace.domain.board.service.ContentBlockService;
-import com.huyeon.superspace.domain.board.service.TagService;
+import com.huyeon.superspace.domain.board.service.NewBoardService;
+import com.huyeon.superspace.domain.board.service.NewCommentService;
+import com.huyeon.superspace.domain.group.exception.AlreadyExistException;
+import com.huyeon.superspace.domain.group.service.NewGroupService;
 import com.huyeon.superspace.web.annotation.GroupPage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,34 +20,30 @@ import java.util.Map;
 @RequestMapping("/api/workspace")
 @RequiredArgsConstructor
 public class BoardController {
-    private final BoardService boardService;
-    private final TagService tagService;
-    private final CommentService commentService;
-    private final ContentBlockService blockService;
+    private final NewGroupService groupService;
+    private final NewBoardService boardService;
+    private final NewCommentService commentService;
 
     @GroupPage
     @GetMapping("/{groupUrl}/board/{id}")
     public Map<String, Object> boardPage(
             @RequestHeader("X-Authorization-Id") String userEmail,
             @PathVariable String groupUrl,
-            @PathVariable Long id) {
+            @PathVariable String id
+    ) {
+        if (groupService.isNotMemberByUrl(groupUrl, userEmail)) {
+            throw new AlreadyExistException("멤버가 아닙니다!");
+        }
 
         Map<String, Object> response = new HashMap<>();
 
-        BoardDto boardResponse = boardService.getBoardDto(id);
+        BoardDto board = boardService.getBoard(id);
 
-        List<TagDto> tags = tagService.getTagResponseByBoardId(boardResponse.getId());
-        boardResponse.setTags(tags);
+        String groupId = groupService.findGroupByUrl(groupUrl).getId();
+        List<CommentDto> comments = commentService.getCommentInBoard(groupId, userEmail, board.getId(), 0);
 
-        List<CommentDto> comments = commentService.getCommentsResponseByBoardId(boardResponse.getId());
-        boardResponse.setComments(comments);
-
-        List<ContentDto> contents = blockService.getContentResponseByBoardId(boardResponse.getId());
-        boardResponse.setContents(contents);
-
-        boardResponse.setUrl("/workspace/" + groupUrl + "/board/" + id);
-
-        response.put("board", boardResponse);
+        response.put("board", board);
+        response.put("comments", comments);
 
         return response;
     }
