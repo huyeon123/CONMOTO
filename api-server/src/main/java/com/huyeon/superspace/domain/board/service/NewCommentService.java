341f8d292300ada560cb3da92a2d092536388f71
Y.Hun <ploki753@gmail.com>
2023-01-26 17:33:46 +0900

    fix: Change to Custom Exception
    - Exception Handling을 세부적으로 하기 위해 제작한 Exception으로 대체

--- api-server/src/main/java/com/huyeon/superspace/domain/board/service/NewCommentService.java
+++ api-server/src/main/java/com/huyeon/superspace/domain/board/service/NewCommentService.java
@@ -6,13 +6,12 @@ import com.huyeon.superspace.domain.board.dto.CommentDto;
 import com.huyeon.superspace.domain.board.dto.CommentPreviewDto;
 import com.huyeon.superspace.domain.board.repository.NewBoardRepository;
 import com.huyeon.superspace.domain.board.repository.NewCommentRepository;
+import com.huyeon.superspace.global.exception.PermissionDeniedException;
 import com.huyeon.superspace.domain.group.repository.NewMemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
@@ -73,7 +72,7 @@ public class NewCommentService {
         Comment origin = commentRepository.findById(request.getId()).orElseThrow();
 
         if (!origin.getAuthor().equals(userEmail)) {
-            throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
+            throw new PermissionDeniedException("작성자 본인만 수정할 수 있습니다!");
         }
 
         origin.setBody(request.getBody());
