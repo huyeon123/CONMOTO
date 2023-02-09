0be446433865608bcf6466ecd8e1ac13a5e2a15d
Y.Hun <ploki753@gmail.com>
2023-02-09 16:11:07 +0900

    fix: Modify Redirect URL & Save RefreshToken

--- auth-server/src/main/java/com/huyeon/authserver/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ auth-server/src/main/java/com/huyeon/authserver/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -4,6 +4,7 @@ import com.huyeon.authserver.auth.dto.UserTokenInfo;
 import com.huyeon.authserver.oauth.repository.HttpCookieOAuth2AuthorizationRequestRepository;
 import com.huyeon.authserver.utils.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 
 @Component
@@ -19,6 +21,7 @@ import java.io.IOException;
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final TokenProvider tokenProvider;
     private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+    private final StringRedisTemplate redisTemplate;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
@@ -36,12 +39,19 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         UserTokenInfo tokenInfo = tokenProvider.createToken(authentication);
 
-        return UriComponentsBuilder.fromUriString("/auth/oauth2/success")
+        saveRedis(authentication.getName(), tokenInfo.getRefreshToken());
+
+        return UriComponentsBuilder.fromUriString("http://localhost:8000/auth/oauth2/success")
                 .queryParam("accessToken", tokenInfo.getAccessToken())
                 .queryParam("accessTokenExpireTime", tokenInfo.getAccessTokenExpireTime())
                 .queryParam("refreshToken", tokenInfo.getRefreshToken())
                 .queryParam("refreshTokenExpireTime", tokenInfo.getRefreshTokenExpireTime())
                 .build().toUriString();
+    }
+
+    private void saveRedis(String email, String refreshToken) {
+        redisTemplate.opsForValue()
+                .set("RefreshToken:" + email, refreshToken, 7, TimeUnit.DAYS);
     }
 
     protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
