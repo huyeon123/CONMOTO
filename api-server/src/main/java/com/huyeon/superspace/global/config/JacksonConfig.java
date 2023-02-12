ad1e5ecfe2e1fbe183512a7f6600caa22ceefab5
Y.Hun <ploki753@gmail.com>
2023-02-12 18:11:57 +0900

    fix: Add JavaTimeModule in ObjectMapper

--- api-server/src/main/java/com/huyeon/superspace/global/config/JacksonConfig.java
+++ api-server/src/main/java/com/huyeon/superspace/global/config/JacksonConfig.java
@@ -1,46 +1,18 @@
 package com.huyeon.superspace.global.config;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class JacksonConfig {
     @Bean
-    public Module jsonLocalDateTimeMapper() {
-        SimpleModule module = new SimpleModule();
-
-        module.addSerializer(LocalDate.class, new JsonSerializer<>() {
-            @Override
-            public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-                gen.writeString(DateTimeFormatter.ofPattern("yyyy-MM-dd").format(value));
-            }
-        });
-
-        module.addSerializer(LocalTime.class, new JsonSerializer<>() {
-            @Override
-            public void serialize(LocalTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-                gen.writeString(DateTimeFormatter.ofPattern("kk:mm:ss").format(value));
-            }
-        });
-
-        module.addSerializer(LocalDateTime.class, new JsonSerializer<>() {
-            @Override
-            public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-                gen.writeString(DateTimeFormatter.ofPattern("yyyy-MM-dd kk:mm:ss").format(value));
-            }
-        });
-
-        return module;
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
     }
 }
