eef5f660e8aaa5cb3455c597f43b2a8a33d73c23
Y.Hun <ploki753@gmail.com>
2023-02-12 17:48:59 +0900

    style: Redesign Sidebar

--- front-server/src/main/resources/static/js/workspace/workspace.js
+++ front-server/src/main/resources/static/js/workspace/workspace.js
@@ -2,20 +2,8 @@ function moveToCategoryPage(obj) {
     window.location.href = "/workspace/" + groupUrl + "/" + obj.outerText.substring(2);
 }
 
-function moveToCreateGroupPage() {
-    window.location.href = "/workspace/new";
-}
-
-function moveToGroupManagingPage(url) {
-    window.location.href = "/workspace/" + url + "/manage";
-}
-
-function moveToCreateCategoryPage() {
-    window.location.href = "/workspace/" + groupUrl + "/new-category";
-}
-
-function moveToManageCategoryPage() {
-    window.location.href = "/workspace/" + groupUrl + "/category";
+function moveToGroupManagingPage() {
+    location.href = pathname + "/manage";
 }
 
 function logout() {
@@ -58,15 +46,49 @@ $(document).on('click', '.sidebar-mobile-dimmed', () => {
     $(".sidebar-mobile-dimmed").css('display', 'none');
 })
 
+let sidebarHeaderSize;
+
 $(() => {
+    sidebarHeaderSize = getSidebarHeaderSize() + 25 + 20;
+    const maxHeight = adjustSideBarMainHeight();
+    addGoTopIfOverflow(maxHeight);
     dimmedSideBar();
 })
+
+function adjustSideBarMainHeight() {
+    const viewHeight = window.innerHeight;
+    const maxHeight = viewHeight - sidebarHeaderSize;
+
+    const $scroll = $(".sidebar-menu-scroll");
+    $scroll.css("maxHeight", maxHeight);
+
+    return maxHeight;
+}
+
+function addGoTopIfOverflow(maxHeight) {
+    const $scroll = $(".sidebar-menu-scroll");
+    const height = $scroll.height();
+
+    if (height >= maxHeight - 10) {
+        const $goTop = $(".sidebar-go-top");
+        $goTop.css('display', 'flex');
+        $goTop.click(() => {
+            $scroll.scrollTop(0);
+        })
+
+        const $category = $(".category");
+        $category.css("padding-bottom", "15px");
+        $category.css("border-bottom", "1px solid #eee");
+    }
+}
 
 $(window).resize(() => {
     dimmedSideBar();
+    adjustSideBarMainHeight();
 })
 
 function dimmedSideBar() {
+
     const width = window.outerWidth;
 
     if (width < 768) $(".super-space-app__sidebar").hide();
@@ -84,3 +106,12 @@ function offDimmed() {
     const width = window.outerWidth;
     if (width < 768) $(".sidebar-mobile-dimmed").hide();
 }
+
+function getSidebarHeaderSize() {
+    return $(".sidebar > header").height();
+}
+
+$(".sidebar-header .expand").click((e) => {
+    const $main = $(e.target).parents(".sidebar-section").find("main");
+    $main.toggle();
+})
