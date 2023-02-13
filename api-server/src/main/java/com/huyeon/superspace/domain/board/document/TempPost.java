8280013e735f8bfa15c374e7dc691284a37d611a
Y.Hun <ploki753@gmail.com>
2023-02-13 21:42:20 +0900

    feat: Add TempPost Document

---
+++ api-server/src/main/java/com/huyeon/superspace/domain/board/document/TempPost.java
@@ -0,0 +1,66 @@
+package com.huyeon.superspace.domain.board.document;
+
+import com.huyeon.superspace.domain.board.dto.BoardDto;
+import com.huyeon.superspace.domain.board.dto.ContentDto;
+import com.huyeon.superspace.global.model.DocumentAudit;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "temp_post")
+public class TempPost extends DocumentAudit {
+    @Id
+    private String id;
+
+    @Setter
+    private String author;
+
+    private String title;
+
+    private String description;
+
+    @Field("group_url")
+    private String groupUrl;
+    @Field("category_name")
+    private String categoryName;
+
+    private String status;
+
+    private String[] tags;
+
+    private TempContent content;
+
+    public TempPost(BoardDto dto) {
+        id = dto.getId();
+        author = dto.getAuthor();
+        title = dto.getTitle();
+        description = dto.getDescription();
+        groupUrl = dto.getGroupUrl();
+        categoryName = dto.getCategoryName();
+        status = dto.getStatus();
+        tags = dto.getTags();
+
+        if (Objects.nonNull(dto.getContent())) {
+            content = new TempContent(dto.getContent());
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    static class TempContent {
+        private String markdown;
+
+        public TempContent(ContentDto contentDto) {
+            markdown = contentDto.getMarkdown();
+        }
+    }
+}
