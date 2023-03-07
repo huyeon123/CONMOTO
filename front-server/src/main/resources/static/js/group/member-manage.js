0820c09130b61b0c7e81ab67ce5094bd9eab310f
Y.Hun <ploki753@gmail.com>
2023-03-07 20:42:55 +0900

    Impl: member-manage.js

--- front-server/src/main/resources/static/js/group/member-manage.js
+++ front-server/src/main/resources/static/js/group/member-manage.js
@@ -1,25 +1,27 @@
+//검색창 입력시 필터링 이벤트 감지
+$("#member-search").keyup(showSearchResults);
+
+//데이터 필터링
+function showSearchResults() {
+    const searchText = $("#member-search").val();
+    const seeAll = (searchText === "");
+
+    const regex = new RegExp(searchText, "gi");
+
+    //멤버들 중 닉네임 혹은 이메일이 검색어와 일치하면 해당 멤버 엘리먼트만 display
+    $("ul.scrollable .member-info").each((idx, element) => {
+        const $element = $(element);
+        if (seeAll) {
+            $element.show();
+        } else {
+            const itemText = $element.find(".js-member-nickname-email").text();
+            if (itemText.match(regex)) $element.show();
+            else $element.hide();
+        }
+    });
 }
 
-}
-
-function activateExpelMemberBtn() {
-    $(".js-expel-btn")
-        .attr("disabled", false)
-        .css("opacity", 1);
-
-    $('#activate-expel')
-        .html("멤버 강퇴 취소")
-        .attr("class", "default-style-btn default")
-        .attr("onclick", "cancelExpelMemberBtn()");
-}
-
-function cancelExpelMemberBtn() {
-    $(".js-expel-btn")
-        .attr("disabled", true)
-        .css("opacity", 0.5);
-
-    $('#activate-expel')
-        .html("멤버 강퇴 활성화")
-        .attr("class", "default-style-btn warn")
-        .attr("onclick", "activateExpelMemberBtn()");
+function moveToMemberPage(elementId) {
+    const id = elementId.slice("member_".length);
+    location.href = "./members/" + id;
 }
