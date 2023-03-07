06212bd9ab9b37e72c9d61cd012dd56538287bab
Y.Hun <ploki753@gmail.com>
2023-03-07 20:00:52 +0900

    fix: Delete @PurePage
    - 어노테이션 역할 변경으로 인한 삭제

--- front-server/src/main/java/com/huyeon/frontend/controller/HomeController.java
+++ front-server/src/main/java/com/huyeon/frontend/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.huyeon.frontend.controller;
 
-import com.huyeon.frontend.aop.PurePage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -10,23 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-    @PurePage
     @GetMapping("/")
     public String mainPage() {
         return "pages/home/main";
     }
 
-    @PurePage
     @GetMapping("/login")
     public String loginPage() {
         return "pages/home/login";
     }
-    @PurePage
     @GetMapping("/signup")
     public String signUpPage() {
         return "pages/home/signup";
     }
-    @PurePage
     @GetMapping("/reset")
     public String resetPasswordPage() {
         return "pages/home/resetpassword";
