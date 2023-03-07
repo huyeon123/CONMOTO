b9e4ba8a7a984c0b11e38842668f623196d67846
Y.Hun <ploki753@gmail.com>
2023-03-07 20:30:44 +0900

    feat: Add saveCategory
    - 현재 보이는대로 카테고리 정보 저장

--- front-server/src/main/resources/static/js/category/category-manage.js
+++ front-server/src/main/resources/static/js/category/category-manage.js
@@ -24,8 +24,41 @@ function fetchGrade() {
         .then(data => levelList = data.levelList);
 }
 
+//카테고리 정보 전체 저장
+function saveCategory() {
+    const url = "/api/category/save?url=" + groupUrl;
+    const request = [];
 
+    const $categoryItem = $("#sortable").children();
 
+    $categoryItem.each((index, item) => {
+        const $item = $(item);
+        const requestElem = {
+            groupUrl: groupUrl,
+            name: $($item.find(".js-category-title")).text(),
+            description: $item.attr("data-description"),
+            type: $item.attr("data-category-type")
+        }
+
+        if (requestElem.type === "CATEGORY_GROUP") {
+            requestElem.fold = $($item.find("input:checkbox")).is(":checked");
+        } else {
+            requestElem.indent = $item.attr("data-indent") === "true";
+            requestElem.availableWriteLevel = $item.attr("data-write-level");
+            requestElem.availableCommentLevel = $item.attr("data-comment-level");
+            requestElem.availableReadLevel = $item.attr("data-read-level");
+        }
+
+        request.push(requestElem);
+    })
+
+    post(url, request)
+        .then((res) => {
+            if (res.ok) {
+                alert("정상 반영되었습니다.");
+                location.href = "/community/" + groupUrl;
+            }
+        })
         .catch(error => {
             alert("저장에 실패했습니다!");
             console.error(error)
