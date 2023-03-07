2a8d9b88aadf35597240bc814f1b5a1b06ed795b
Y.Hun <ploki753@gmail.com>
2023-03-07 20:25:08 +0900

    feat: Add drawPosts
    - 그룹 메인 페이지, 카테고리 페이지, 멤버 페이지 등에서 여러 게시글을 렌더링할 수 있는 메서드

--- front-server/src/main/resources/static/js/board/board-util.js
+++ front-server/src/main/resources/static/js/board/board-util.js
@@ -6,17 +6,51 @@ const getList = (url) => {
     return get(url);
 };
 
+function drawBoards(data) {
+    if (Array.isArray(data)) {
+        drawEachItem(data)
+    } else {
+        for (let value of Object.values(data)) {
+            drawEachItem(value);
         }
+    }
+
+    isFetching = false;
 }
 
+function drawEachItem(data) {
     data.forEach((item, index) => {
         drawBoard(item);
 
+        setLastIndex(index, data);
     });
 }
 
 function drawBoard(item) {
+    const $element = $(`
+        <li id="${item.id}" class="article pointer-hover" onclick="moveToBoardPage(this.id)">
+                    <div class="article-body-wrapper">
+                        <div class="article-preview-wrapper">
+                            <div class="article-title">${item.title}</div>
+                            <div class="article-description">${item.description}</div>
+                        </div>
+                        <div class="article-thumbnail">
+                            <img src="/img/user.svg" alt="thumbnail" class="article-thumbnail-img">
+                        </div>
+                    </div>
+                    <div class="article-detail-wrapper">
+                        <span>${item.author}</span>
+                        <span>${item.lastUpdate}</span>
+                    </div>
+                </li>
+    `);
+
+    $("#draw-board-container").append($element);
+}
+
+function setLastIndex(index, data) {
     if (index === data.length - 1) {
+        lastIndex = data[index].id;
     }
 }
 
