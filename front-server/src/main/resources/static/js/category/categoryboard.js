40d844febff180aaf0b7a2ea08a29cf38359a085
Y.Hun <ploki753@gmail.com>
2023-03-07 20:32:42 +0900

    feat: Add Category Page Rendering

--- front-server/src/main/resources/static/js/category/categoryboard.js
+++ front-server/src/main/resources/static/js/category/categoryboard.js
@@ -1,10 +1,39 @@
+let lastIndex = Number.MAX_SAFE_INTEGER;
 
 function pageRender() {
+    const url = createAPIURL();
+    renderPost(url, "작성된 게시글이 없습니다.");
 }
 
+function createAPIURL() {
+    const url = new URL(location.origin + "/api/board/latest");
+    const params = {
+        "lastIndex": lastIndex,
+        "categoryId": $("#draw-board-container").data("category-id")
+    };
 
+    for (let key in params) {
+        url.searchParams.set(key, params[key]);
+    }
+
+    return url;
+}
+
+function renderPost(url, nothing_body) {
     getList(url)
         .then(res => res.json())
-        .then(data => drawBoards(data))
-        .catch(error => console.error(error));
+        .then(data => {
+            if (Object.keys(data).length === 0) {
+                renderHasNothing(nothing_body);
+            }
+            data.forEach((item, index) => {
+                drawBoards(item);
+                setLastIndex(index, data);
+            })
+        });
+}
+
+function renderHasNothing(body) {
+    const $element = $(`<div class="nothing">${body}</div>`);
+    $("#draw-board-container").append($element);
 }
