e608e9815d190f57d1d223aaff21cfa85d4affdf
Y.Hun <ploki753@gmail.com>
2023-03-07 21:29:58 +0900

    feat: Add MemberLikePost
    - 멤버가 좋아요를 누른 게시글을 추적하기 위한 Document

---
+++ api-server/src/main/java/com/huyeon/superspace/domain/board/document/MemberLikePost.java
@@ -0,0 +1,75 @@
+package com.huyeon.superspace.domain.board.document;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Document(collection = "member_like_post")
+public class MemberLikePost {
+    @Id
+    private String id; //Member Id와 공유
+
+    private List<LikePostInfo> likes;
+
+    public MemberLikePost(String id) {
+        this.id = id;
+        likes = new ArrayList<>();
+    }
+
+    public List<LikePostInfo> getNextLikes(Long lastIndex, int page) {
+        final int SIZE = 50;
+        if (lastIndex > likes.size()) lastIndex = (long) likes.size();
+        lastIndex -= ((long) SIZE * page);
+
+        List<LikePostInfo> next = new LinkedList<>();
+        for (long i = lastIndex - 1; i >= lastIndex - SIZE && i >= 0; i--) {
+            next.add(likes.get((int) i));
+        }
+
+        return next;
+    }
+
+    public void addLikePost(Long postId) {
+        likes.add(new LikePostInfo(postId));
+    }
+
+    public void removeLikePost(Long postId) {
+        likes.remove(new LikePostInfo(postId));
+    }
+
+    @Getter
+    @Setter
+    public static class LikePostInfo {
+        private Long postId;
+        private LocalDateTime timestamp;
+
+        public LikePostInfo(Long postId) {
+            this.postId = postId;
+            this.timestamp = LocalDateTime.now();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            LikePostInfo that = (LikePostInfo) o;
+
+            return Objects.equals(postId, that.postId);
+        }
+
+        @Override
+        public int hashCode() {
+            return postId != null ? postId.hashCode() : 0;
+        }
+    }
+}
