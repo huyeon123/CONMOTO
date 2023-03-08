27a479ea687c669941db71870858a06d1f1359de
Y.Hun <ploki753@gmail.com>
2023-03-08 11:35:00 +0900

    fix: Modify Type in ContentUpdateDto

--- api-server/src/main/java/com/huyeon/superspace/domain/board/dto/ContentUpdateDto.java
+++ api-server/src/main/java/com/huyeon/superspace/domain/board/dto/ContentUpdateDto.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class ContentUpdateDto {
-    private String boardId;
+    private Long boardId;
     private String markdown;
 }
