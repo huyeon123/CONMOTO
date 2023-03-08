c47c8e471a9dfd9fae7b4c76cdaa1cccba52cf15
Y.Hun <ploki753@gmail.com>
2023-03-08 12:29:50 +0900

    fix: findNextNoty
    - JPQL을 활용하여 lastIndex보다 낮은 ID를 갖는 알림만 조회하도록 변경

--- api-server/src/main/java/com/huyeon/superspace/domain/noty/repository/NotyReceiverRepository.java
+++ api-server/src/main/java/com/huyeon/superspace/domain/noty/repository/NotyReceiverRepository.java
@@ -3,6 +3,8 @@ package com.huyeon.superspace.domain.noty.repository;
 import com.huyeon.superspace.domain.noty.entity.ReceivedNoty;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,9 @@ import java.util.List;
 public interface NotyReceiverRepository extends JpaRepository<ReceivedNoty, Long> {
     List<ReceivedNoty> findAllByUserEmail(String userEmail);
 
-    List<ReceivedNoty> findAllByUserEmailOrderByIdDesc(String userEmail, Pageable pageable);
+    @Query("select n from ReceivedNoty n where id < :lastIndex and userEmail = :userEmail")
+    List<ReceivedNoty> findNextNoty(
+            @Param("userEmail")String userEmail,
+            @Param("lastIndex") Long lastIndex,
+            Pageable pageable);
 }
