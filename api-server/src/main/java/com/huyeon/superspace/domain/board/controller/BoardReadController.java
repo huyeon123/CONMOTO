3750de6b0beee3d3c366b94153b0d67a5395e073
Y.Hun <ploki753@gmail.com>
2023-03-08 12:00:17 +0900

    refactor: Simplify getNextLatest API

--- api-server/src/main/java/com/huyeon/superspace/domain/board/controller/BoardReadController.java
+++ api-server/src/main/java/com/huyeon/superspace/domain/board/controller/BoardReadController.java
@@ -3,13 +3,15 @@ package com.huyeon.superspace.domain.board.controller;
 import com.huyeon.superspace.domain.board.document.TempPost;
 import com.huyeon.superspace.domain.board.dto.BoardDto;
 import com.huyeon.superspace.domain.board.dto.ContentDto;
-import com.huyeon.superspace.domain.board.service.NewBoardService;
+import com.huyeon.superspace.domain.board.service.*;
+import com.huyeon.superspace.global.exception.BadRequestException;
 import com.huyeon.superspace.global.exception.PermissionDeniedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -17,35 +19,67 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BoardReadController {
 
+    private final NewCategoryService categoryService;
     private final NewBoardService boardService;
+    private final PopularBoardService popularBoardService;
+
+    private final NewCommentService commentService;
 
     @GetMapping("/{id}")
     public BoardDto getBoard(@PathVariable Long id) {
         return boardService.getBoard(id);
     }
 
-    @GetMapping("/latest/group")
+    @GetMapping("/latest")
     public List<BoardDto> getLatestBoardInGroup(
+            @RequestParam String categoryId,
+            @RequestParam Long lastIndex,
+            @RequestParam(required = false, defaultValue = "0") int page
+    ) {
+        return boardService.getNextCategory(categoryId, lastIndex, page);
+    }
+
+    @GetMapping("/latest/all")
+    public List<BoardDto> getNextGroup(
             @RequestParam String url,
-            @RequestParam int page
+            @RequestParam Long lastIndex,
+            @RequestParam(required = false, defaultValue = "0") int page
     ) {
-        return boardService.getNext10LatestInGroup(url, page);
+        return boardService.getNextGroup(url, lastIndex, page);
     }
 
-    @GetMapping("/latest/category")
-    public List<BoardDto> getLatestBoardInCategory(
-            @RequestParam String name,
-            @RequestParam int page
+    @GetMapping("/latest/notice")
+    public List<BoardDto> getNextNotice(
+            @RequestParam String url,
+            @RequestParam Long lastIndex,
+            @RequestParam(required = false, defaultValue = "0") int page
     ) {
-        return boardService.getNext10LatestInCategory(name, page);
+        String noticeId = categoryService.getCategoryByName(url, "⭐공지사항").getId();
+        return boardService.getNextNotice(noticeId, lastIndex, page);
     }
 
-    @GetMapping("/latest/mine")
+    @GetMapping("/latest/popular")
+    public Map<String, Object> getPopularBoards(@RequestParam String url) {
+        return popularBoardService.getPopularBoards(url);
+    }
+
+    @GetMapping("/latest/member")
     public List<BoardDto> getLatestBoardInUser(
-            @RequestHeader("X-Authorization-Id") String userEmail,
-            @RequestParam int page
+            @RequestParam String memberId,
+            @RequestParam String type,
+            @RequestParam Long lastIndex,
+            @RequestParam(required = false, defaultValue = "0") int page
     ) {
-        return boardService.getNext10LatestInUser(userEmail, page);
+        switch (type) {
+            case "write":
+                return boardService.getNextMember(memberId, lastIndex, page);
+            case "commented":
+                return commentService.getNextCommentedPosts(memberId, lastIndex, page);
+            case "like":
+                return likePostService.getNextLike(memberId, lastIndex, page);
+            default:
+                throw new BadRequestException("잘못된 type 요청입니다.");
+        }
     }
 
     @GetMapping("/content/{id}")
