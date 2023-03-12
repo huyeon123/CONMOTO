e97ff2c05a568b90803dd9917849feb05dd6d5ae
Y.Hun <ploki753@gmail.com>
2023-03-12 21:51:01 +0900

    feat: Add ObjectStorageConfig

---
+++ cdn-server/src/main/java/com/huyeon/cdn/config/ObjectStorageConfig.java
@@ -0,0 +1,29 @@
+package com.huyeon.cdn.config;
+
+import com.oracle.bmc.ConfigFileReader;
+import com.oracle.bmc.Region;
+import com.oracle.bmc.auth.AbstractAuthenticationDetailsProvider;
+import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
+import com.oracle.bmc.objectstorage.ObjectStorage;
+import com.oracle.bmc.objectstorage.ObjectStorageClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class ObjectStorageConfig {
+    @Value("${oci.configPath}")
+    private String CONFIG_FILE_PATH;
+
+    @Bean
+    public ObjectStorage objectStorage() throws IOException {
+        ConfigFileReader.ConfigFile configFile = ConfigFileReader.parse(CONFIG_FILE_PATH);
+        AbstractAuthenticationDetailsProvider provider = new ConfigFileAuthenticationDetailsProvider(configFile);
+
+        return ObjectStorageClient.builder()
+                .region(Region.AP_CHUNCHEON_1)
+                .build(provider);
+    }
+}
